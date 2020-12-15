121e6ed3da5df6504ed1b369d4abdf2f56474161
Arsh Sharma <arshsharma461@gmail.com>
2020-12-15 16:26:22 +0530

    initial commit

---
+++ hello-world/src/components/header.js
@@ -0,0 +1,7 @@
+import React from "react"
+
+function header(props) {
+  return <h1 style={{ color: "red" }}>{props.headerText}</h1>
+}
+
+export default header
---
+++ hello-world/src/pages/about.js
@@ -0,0 +1,12 @@
+import React from "react"
+import Header from "../components/header"
+
+function about() {
+  return (
+    <div>
+      <Header headerText="YOLO mates" />
+    </div>
+  )
+}
+
+export default about
