2f29360d0461dd37212f85b5f6136ce5690a0a73
Arsh Sharma <arshsharma461@gmail.com>
2020-12-20 17:56:21 +0530

    added data from site info

---
+++ working-with-data/gatsby-config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  siteMetadata: {
+    title: `Who Let The Pandas Out`,
+  },
+  plugins: [
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `src`,
+        path: `${__dirname}/src/`,
+      },
+    },
+    `gatsby-plugin-emotion`,
+    {
+      resolve: `gatsby-plugin-typography`,
+      options: {
+        pathToConfigModule: `src/utils/typography`,
+      },
+    },
+  ],
+}
